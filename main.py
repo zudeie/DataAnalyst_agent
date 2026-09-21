1423100f884fb14d8df18d9e53c58e3e2a45967e
zudeie <159454585+zudeie@users.noreply.github.com>
2026-09-21 20:44:59 +0545

    Built Graph and invoke func

--- main.py
+++ main.py
@@ -3,37 +3,46 @@ from langgraph.prebuilt import ToolNode
 from langgraph.graph.message import add_messages
 from typing import Annotated, TypedDict
 from pydantic import BaseModel
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_ollama import ChatOllama   
 from tools import query_database
 from dotenv import load_dotenv
 
 load_dotenv()
-class State(BaseModel):
-    user_input: str
+class Analyst_State(BaseModel):
+    chart_json:str =""
     messages: Annotated[list[BaseMessage], add_messages]
 
 tools = [query_database]
 
-llmwtools = ChatOllama(model="qwen3:14b",
-                       temperature=0,
+Analyst_agent = ChatOllama(model="qwen3:14b",
+                       temperature=0.1,
                        num_ctx=8192,).bind_tools(tools)
 
-def chatbot(state: State):
-    return {"messages": [llmwtools.invoke(state["messages"])]}
+def chatAnalyst(state: Analyst_State):
+    response = Analyst_agent.invoke(state.messages)
+    state.messages.append(response)
+    return state
+    
 
-def should_continue(state: State):
+def routing(state: Analyst_State):
     last_message = state["messages"][-1]
     if last_message.tool_calls:
         return "tools"
     return END
 
-graph = StateGraph(State)
-graph.add_node("chatbot", chatbot)
+graph = StateGraph(Analyst_State)
+graph.add_node("chatAnalyst", chatAnalyst)
 graph.add_node("tools", ToolNode(tools))
 
-graph.add_edge(START, "chatbot")
-graph.add_conditional_edges("chatbot", should_continue, ["tools", END])
-graph.add_edge("tools", "chatbot")
+graph.add_edge(START, "chatAnalyst")
+graph.add_conditional_edges("chatAnalyst", routing, ["tools", END])
+graph.add_edge("tools", "chatAnalyst")
 
-agent = graph.compile()
+Analyst__agent = graph.compile()
+
+def invoke_agent(messg:str):
+    result = Analyst__agent.invoke(HumanMessage(content=messg))
+    return result["messages"][-1].content
+
+# def stream(mssg:str,"")
